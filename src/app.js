62d2969d54ef64114453804759ac0fdab5d1043d
Samuel Gascon <2151866@cstj.qc.ca>
2022-11-22 09:37:05 -0500

    app.use()

--- src/app.js
+++ src/app.js
@@ -5,13 +5,18 @@ import database from './libs/database.js';
 
 import errorMiddleware from './middlewares/errors.js';
 
-
+import pizzeriaRoutes from './routes/pizzeria-routes.js'
+import ordersRoutes from './routes/order-routes.js'
+import customersRoutes from './routes/customers-routes.js'
 database();
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/pizzeria', pizzeriaRoutes);
+app.use('/order', ordersRoutes);
+app.use('/customer', customersRoutes);
 
 app.use(errorMiddleware);
 
