d9fdc2a840a82c95726be07be138fd63bd0bf2d8
Sgascon-cstj <89391189+Sgascon-cstj@users.noreply.github.com>
2022-11-25 09:26:32 -0500

    added customers transfomr

--- src/repositories/customers-repository.js
+++ src/repositories/customers-repository.js
@@ -1,8 +1,23 @@
 
+import dayjs from "dayjs";
 import customerModel from "../models/customer-model.js";
 
 class customersRepository {
 
+    retieve(filter)
+    {
+        return customerModel.find(filter);
+    }
+
+    transform(customers){
+        customers.href = `${process.env.BASE_URL}/customers/${customers._id}`;
+        customers.lightSpeed = `[${customers.planet}]@(${customers.coord.lat};${customers.coord.lon})`;
+
+        customers.age = dayjs().subtract(dayjs(customers.birthday).year(),'year').year();
+        customers.phone = `[${customers.phone.substring(0,4)}]${customers.phone.substring(4,8)}-${customers.phone.substring(8)}`;
+
+        return customers;
+    }
   
 
 }
