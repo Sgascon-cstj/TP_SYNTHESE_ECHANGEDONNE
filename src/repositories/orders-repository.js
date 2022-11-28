cd7c714223c6b3a215e50431ed73f21ec8d9aadf
Tom Strohmeier <1232614@cstj.qc.ca>
2022-11-28 13:02:52 -0500

    Added transform
    , work in progress of getOne and transform, debug avec sam

--- src/repositories/orders-repository.js
+++ src/repositories/orders-repository.js
@@ -3,8 +3,34 @@ import orderModel from "../models/order-model.js";
 
 class OrdersRepository {
 
-  retrieveAll(){
+  retrieveAll() {
     return orderModel.find();
+  }
+
+  retrieveById(idPizzeria, idOrder, retrieveOptions) {
+    const retrieveOrder = orderModel.find({ pizzeria: idPizzeria, _id: idOrder });
+
+    if (retrieveOptions.customer) {
+      retrieveOrder.populate('customer');
+    }
+
+    return retrieveOrder;
+  }
+
+  transform(order) {
+    order.href = `${process.env.BASE_URL}/orders/${order._id}`;
+    order.customer = `${process.env.BASE_URL}/customers/${order.customer}`;
+    order.pizzeria = `${process.env.BASE_URL}/pizzerias/${order.pizzeria}`;
+
+    order.pizzas.forEach(p => {
+      delete p.topping;
+      delete p._id;
+      delete p.id;
+    });
+
+    delete order._id;
+    delete order.id;
+    return order;
   }
 
 }
